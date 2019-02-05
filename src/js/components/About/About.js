f98012be7d11ca2b835f50d07af5750451821842
Ryan Spink <ryanspinktech@gmail.com>
2019-02-04 23:54:55 -0700

    finished

--- src/js/components/About/About.js
+++ src/js/components/About/About.js
@@ -19,65 +19,80 @@ class ConnectedAbout extends Component{
   }
 
   render(){
+    const aboutText =
+    <div>
+      <p>
+          t<span style={{color:'red'}}><sup>3</sup></span>'s investment process beings with qualitative and quantitative research accumulated from
+          various sources including tradition wire-house research. The process utilizes a building block approach to model development.
+          The process relies on a multi-factor algorithm that starts with a proprietary foundation factor that is then over-laid with
+          traditional qualitative and quantitative factors (value and growth) confirming with technical overlays.
+      </p>
+      <p>
+          t<span style={{color:'red'}}><sup>3</sup></span> offers various portfolios that are uniquely designed to meet different client objectives.
+           All models are built off of the same platform. The particular universe for each portfolio varies according to the risk objectives of each individual client.
+            Currently t3 offers the following portfolios:
+      </p>
+      <ul>
+        <li style={{listStyleType:'none'}}>
+          <p style={{textIndent:'0'}}>
+            <span style={{color:'red', fontWeight:'bold' , borderBottom:'1px solid red'}}>Large Cap Core:</span><br/>
+            Selections are chosen from the S&P 500 Universe.
+            <br/>
+            (long individual securities, short individual securities, dollar neutral, long-bias, sectors, and enhanced index)
+          </p>
+        </li>
+        <br/>
+        <li style={{listStyleType:'none'}}>
+          <p style={{textIndent:'0'}}>
+            <span style={{color:'red' ,fontWeight:'bold' , borderBottom:'1px solid red'}}>All-Cap Core:</span><br/>
+            Selections are chosen from the Russel 3000 Universe.
+            <br/>
+            (long individual securities, short individual securities, dollar neutral, long-bias, sectors, and enhanced index)
+          </p>
+        </li>
+        <br/>
+        <li style={{listStyleType:'none'}}>
+          <p style={{textIndent:'0'}}>
+            <span style={{color:'red', fontWeight:'bold', borderBottom:'1px solid red'}}>Small-Cap Core:</span><br/>
+            Selections are chosen from the Russel 2000 Universe.
+            <br/>
+            (long individual securities, and enhanced index)
+          </p>
+        </li>
+        <br/>
+      </ul>
+      <p>
+        t<span style={{color:'red'}}><sup>3</sup></span> may also blend various portfolios from the above categories (and within the above categories) into one portfolio
+        in order to uniquely match the risk characteristics of a particular investor.
+      </p>
+      <p>
+        All portfolios are composed of concentrated sector and stock selections. All stocks are evenly weighed at the time of construction and are re-balanced quarterly. All stocks are evaluated according to solid academic and scientific principles. All stocks are evaluated vs. various quantitative factors including, but not limited to: Earnings Expectations, Quality Ratings, Analysts Intermediate Buy Ratings, Stability of Dividend, Dividend Value, P/E ratio, Beta, along with a short and intermediate term technical factor in order to arrive at a final list.
+      </p>
+      <p>
+        Any Initial Public Offering stocks will be distributed only to NASD eligible accounts.
+      </p>
+    </div>
+
     return(
       <>
         <Grid.Row only='computer' centered className='about-row'>
           <Grid.Column  width={12} centered className='about-row-column'>
             <div className='about-text'>
-            <p>
-                t<span style={{color:'red'}}>3</span>equity labs, llc is an innovative equity portfolio research and management firm.  t<span style={{color:'red'}}>3</span> combines 21 years of stock  market
-                experience, traditional fundamental economic principles and a strong foundation of academic finance together to produce
-                a scientifically advanced stock portfolio engine that is designed to manage the downside risk inherent in the stock market.
-            </p>
-            <p>
-                This is done through a proprietary combination of Behavioral Economics, quantitative, fundamental and technical factors into
-                one Quantimental trigger which is controlled and managed through a strict set of rules.
-            </p>
-            <p>
-                The goal is to direct investors
-                into Sectors of the market predicted to benefit from future earnings surprises while avoiding Sectors that will suffer primarily
-                due to negative earnings surprises.
-            </p>
+              { aboutText }
             </div>
           </Grid.Column>
         </Grid.Row>
         <Grid.Row only='tablet' centered className='about-row tab'>
           <Grid.Column  width={14} centered className='about-row-column tab'>
             <div className='about-text'>
-            <p>
-                t<span style={{color:'red'}}>3</span>equity labs, llc is an innovative equity portfolio research and management firm.  t<span style={{color:'red'}}>3</span> combines 21 years of stock  market
-                experience, traditional fundamental economic principles and a strong foundation of academic finance together to produce
-                a scientifically advanced stock portfolio engine that is designed to manage the downside risk inherent in the stock market.
-            </p>
-            <p>
-                This is done through a proprietary combination of Behavioral Economics, quantitative, fundamental and technical factors into
-                one Quantimental trigger which is controlled and managed through a strict set of rules.
-            </p>
-            <p>
-                The goal is to direct investors
-                into Sectors of the market predicted to benefit from future earnings surprises while avoiding Sectors that will suffer primarily
-                due to negative earnings surprises.
-            </p>
+              { aboutText }
             </div>
           </Grid.Column>
         </Grid.Row>
         <Grid.Row only='mobile' centered className='about-row mob'>
           <Grid.Column  width={14} centered className='about-row-column mob'>
             <div className='about-text mob'>
-            <p>
-                t<span style={{color:'red'}}>3</span>equity labs, llc is an innovative equity portfolio research and management firm.  t<span style={{color:'red'}}>3</span> combines 21 years of stock  market
-                experience, traditional fundamental economic principles and a strong foundation of academic finance together to produce
-                a scientifically advanced stock portfolio engine that is designed to manage the downside risk inherent in the stock market.
-            </p>
-            <p>
-                This is done through a proprietary combination of Behavioral Economics, quantitative, fundamental and technical factors into
-                one Quantimental trigger which is controlled and managed through a strict set of rules.
-            </p>
-            <p>
-                The goal is to direct investors
-                into Sectors of the market predicted to benefit from future earnings surprises while avoiding Sectors that will suffer primarily
-                due to negative earnings surprises.
-            </p>
+             { aboutText}
             </div>
           </Grid.Column>
         </Grid.Row>
--- src/js/components/News/News.js
+++ src/js/components/News/News.js
@@ -19,18 +19,48 @@ class ConnectedNews extends Component{
     };
   }
 
+  componentDidMount(){
+    this.checkNewsStatus()
+  }
 
-  componentWillMount(){
+  checkNewsStatus(){
+    if( sessionStorage.getItem("t3NewsArticles") ){
+      let d = Date.now() - sessionStorage.getItem("t3NewsRetrieveTime");
+      (d >= 2000000)
+        ? sessionStorage.clear()
+        : this.retrieveSessionNews()
+    }else{
+      this.getNewsArticles()
+    }
+  }
+
+  retrieveSessionNews(){
+    debugger;
+    let retrievedStories = sessionStorage.getItem("t3NewsArticles")
+    let stories = JSON.parse(retrievedStories)
+    let organizedStories = stories.sort(function(a, b) {
+      a = new Date(a.children[3].value);
+      b = new Date(b.children[3].value);
+      return a>b ? -1 : a<b ? 1 : 0;
+    });
+    this.setState({stories: organizedStories})
+  }
+
+  getNewsArticles(){
+    sessionStorage.clear()
     let companies = ['aapl','alk','ame','avy','blk','cme','cop','de','ecl','ice','jpm','key','nue','pxd','rsg','shw','tel','wfc','wrk','xom']
     let news = []
     companies.forEach((company)=>{Axios.get(`https://peaceful-beach-96299.herokuapp.com/https://feeds.finance.yahoo.com/rss/2.0/headline?s=${company}`)
       .then(response => {
         let self = this;
+
         let xml = new XMLParser().parseFromString(response.data);
         let stories = xml.children[0].children.slice(3,10)
         if(stories.length >=1){
           news = news.concat(stories)
           this.setState({stories:news})
+          sessionStorage.setItem( 't3NewsArticles', JSON.stringify(news)   )
+          sessionStorage.setItem( 't3NewsRetrieveTime', Date.now().toString() )
         }
       })
     })
@@ -42,15 +72,14 @@ class ConnectedNews extends Component{
     let storyList
     if(stories.stories.length >=1){
     storyList = stories.stories.map((story, index) => (
-
       <Grid.Column key={index} className='news-card-col'>
         <Card href={ story.children[2].value } className='home-row-two-card news'>
           <Card.Content>
             <Card.Header className='home-row-two-card-header news' textAlign={'center'}>
-              { story.children[4].value }
+              { story.children[4].value.slice(0,70)+"..." }
             </Card.Header>
             <Card.Meta>
-            { story.children[3].value }
+            { story.children[3].value.slice(0,16) }
             </Card.Meta>
           </Card.Content>
         </Card>
@@ -59,6 +88,12 @@ class ConnectedNews extends Component{
     }
     return(
       <>
+        <Grid.Row columns={1} only='computer tablet'centered>
+          <Grid.Column className='news-title-column'>
+            <span className='news-page-title'>Select Investment News</span>
+          </Grid.Column>
+        </Grid.Row>
+
         <Grid.Row columns={4} only='computer' className='news-rows'>
           { storyList }
         </Grid.Row>
